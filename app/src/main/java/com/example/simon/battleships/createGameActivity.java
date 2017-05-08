1ecc89f53ac2bf3647765dc31ef45fffa4c6c46a
Andy <andysiutang@gmail.com>
2017-05-08 13:55:07 +0200

    PUSH
    YEAH

--- app/src/main/java/com/example/simon/battleships/createGameActivity.java
+++ app/src/main/java/com/example/simon/battleships/createGameActivity.java
@@ -2,10 +2,13 @@ package com.example.simon.battleships;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class createGameActivity extends AppCompatActivity {
 
     Host host;
+    SocketHandler socketHandler;
+    TextView statusText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +16,20 @@ public class createGameActivity extends AppCompatActivity {
 
         host = new Host();
         host.execute();
+        socketHandler = new SocketHandler();
+        statusText = (TextView) findViewById(R.id.statusText);
+
+        while (!socketHandler.hasClientSocket()){
+
+            try {
+                Thread.sleep(500);
+            } catch (Exception e){
+
+            }
+        }
+
+        statusText.setText(socketHandler.getClientSocket().toString() + " has connected");
+
+
     }
 }
--- app/src/main/java/com/example/simon/battleships/joinGameActivity.java
+++ app/src/main/java/com/example/simon/battleships/joinGameActivity.java
@@ -2,9 +2,10 @@ package com.example.simon.battleships;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class joinGameActivity extends AppCompatActivity {
-
+    EditText ipEnter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
